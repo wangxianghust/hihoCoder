614fa3c72d59e78d27bead2811c9b3cca93cadd8
wangxianghust <xianghust@gmail.com>
2017-03-31 14:37:01 +0800

    Pro 1000HelloHiho
    Hello, world!

---
+++ 1000AplusB.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int sum(int a, int b){
+    return a+b;
+}
+
+int main(){
+    int a, b;
+   // cin >> a >> b;
+   // cout << sum(a, b) << endl;
+    while(cin >> a >> b){
+        cout << sum(a, b) << endl;
+    }
+    return 0;
+}
